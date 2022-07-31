68d3ea3ca074233e32d9dc2016fceedcf671e0ce
Vit Kozel <vitek.kozel@email.cz>
2022-07-31 11:01:58 +0200

    nevermind i reverted it

--- Guam/palette.py
+++ Guam/palette.py
@@ -3,63 +3,61 @@ from tkinter import *
 global result
 
 def palette():
+    #global result
     def Scankey(event):
         val = event.widget.get()
-        print(" Current input: " + val)
+        print(' Current input: "' + val + '"')
 
-#global result
-def Scankey(event):
-    val = event.widget.get()
-    print(' Current input: "' + val + '"')
+        if val == '':
+            data = list
+        else:
+            data = []
+            for item in list:
+                if val.lower() in item.lower():
+                    data.append(item)				
+        Update(data)
+        #print(data)
 
-    if val == '':
-        data = list
-    else:
-        data = []
-        for item in list:
-            if val.lower() in item.lower():
-                data.append(item)				
-    Update(data)
-    #print(data)
+    def Update(data): # Update function
+        listbox.delete(0, 'end')
+        # put new data
+        for item in data:
+            listbox.insert('end', item)
 
     def callback(event):
+        global result
         callback = listbox.get(listbox.curselection())
         print(" Palette selected: " + callback)
+        result = callback
         return callback
 
-def callback(event):
-    global result
-    callback = listbox.get(listbox.curselection())
-    print(" Palette selected: " + callback)
-    result = callback
-    return callback
+
+    list = ('Change theme','DISPLAY_DEBUG_SIZE','DISPLAY_FONT_SIZE',
+        'DISPLAY_FONT_TYPEFACE','DEFAULT_FILE_CODING',
+        'CHECK_FILE_CODING','Save','Save As', 'Open' )
+
+    ws = Tk()
+    ws.title('Guam Command Palette')
+    ws.geometry('300x195')
+
+    entry = Entry(ws, width = 300)
+    entry.pack()
+    entry.bind('<KeyRelease>', Scankey)
 
 
-list = ('Change theme','DISPLAY_DEBUG_SIZE','DISPLAY_FONT_SIZE',
-    'DISPLAY_FONT_TYPEFACE','DEFAULT_FILE_CODING',
-    'CHECK_FILE_CODING','Save','Save As', 'Open' )
+    listbox = Listbox(ws, width = 300)
+    listbox.pack()
+    Update(list)
 
-ws = Tk()
-ws.title('Guam Command Palette')
-ws.geometry('300x195')
+    callback = listbox.bind("<<ListboxSelect>>", callback)
+    #print(" Palette selected: " + callback)
 
-entry = Entry(ws, width = 300)
-entry.pack()
-entry.bind('<KeyRelease>', Scankey)
-
-
-listbox = Listbox(ws, width = 300)
-listbox.pack()
-Update(list)
-
-callback = listbox.bind("<<ListboxSelect>>", callback)
-#print(" Palette selected: " + callback)
-
-ws.mainloop()
+    ws.mainloop()
 
 def main():
-    palette()
+    global result
+    print(result)
     return result
 
-#main()
+main()
 #palette()
